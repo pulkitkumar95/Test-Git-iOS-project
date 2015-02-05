2d92677ac7ea43c0bf9958f7424e330175bbba5e
Pulkit Kumar <pulkitkumar95@gamil.com>
2015-02-05 12:11:43 +0530

    Added test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  gitTest
+//
+//  Created by Pulkit Kumar on 05/02/15.
+//  Copyright (c) 2015 Pulkit Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  gitTest
+//
+//  Created by Pulkit Kumar on 05/02/15.
+//  Copyright (c) 2015 Pulkit Kumar. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
